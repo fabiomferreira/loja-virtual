75864567d76ad3bc3a44e49e2579e8387d093c57
Fabio Ferreira <fabiomataferreira@gmail.com>
2017-09-30 23:50:03 -0400

    cabecalho criado

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<!-- <link rel="stylesheet" type="text/css" href="css/bootstrap-reboot.css"> -->
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<title>Loja do Fábio</title>
 </head>
 <body>
+	<div class="header clearfix navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">Loja do Fabio</a>
+		<ul id="navbarsExampleDefault" class="navbar-nav mr-auto">
+			<li class="nav-item active"><a class="nav-link" href="#">Produtos</a></li>
+			<li class="nav-item"><a class="nav-link" href="cadastra-produto.php">Cadastrar produto</a></li>
+		</ul>
+
+	</div>
+
+	<div class="container">
+		<div class="jumbotron">
+			<h1 class="display-3">Bem vindo!</h1>
+			<p class="lead">Seja bem-vindo(a) à melhor loja da internet</p>
+		</div>		
+	</div>
 
 </body>
 </html>
